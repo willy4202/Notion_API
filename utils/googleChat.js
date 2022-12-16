bd167e4079edcd9f28fc65b2218b762690848c48
young <willy4202@naver.com>
2022-12-16 16:50:34 +0900

    add: google chat api

--- utils/googleChat.js
+++ utils/googleChat.js
@@ -1,34 +1,24 @@
-const { google } = require('googleapis');
-const chat = google.chat('v1');
 const fetch = require('node-fetch');
 
-const auth = new google.auth.GoogleAuth({
-  keyFile: '../notion-api-test-371502-111a674b761a.json',
-  scopes: ['https://www.googleapis.com/auth/chat.bot'],
-});
+const notion_webhook =
+  'https://chat.googleapis.com/v1/spaces/AAAATrMPwY8/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=Gku50H4kLKlh0jRjTWncSfV9Mip1Pa57r12LQDmCNWY%3D';
 
-/**
- * Sends asynchronous message into Google Chat
- */
 async function sendMessageToSpace(message) {
-  const webhookURL =
-    'https://chat.googleapis.com/v1/spaces/AAAATrMPwY8/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=Gku50H4kLKlh0jRjTWncSfV9Mip1Pa57r12LQDmCNWY%3D';
-
   const data = JSON.stringify({
     text: message,
   });
-  let resp;
-  fetch(webhookURL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json; charset=UTF-8',
-    },
-    body: data,
-  }).then((response) => {
-    resp = response;
-    console.log(response);
-  });
-  return resp;
+  try {
+    let response = await fetch(notion_webhook, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: data,
+    });
+    return response;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 module.exports = { sendMessageToSpace };
